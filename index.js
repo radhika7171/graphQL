e48c8891ce751f59021721be55b71c9620590fd7
radhika7171 <aairamahajan@gmail.com>
2022-03-21 19:26:21 -0400

    graphQl inplemented

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Start your es6 scripts here
+import express from "express";
+import resolvers from "./resolvers";
+import schema from "./schema";
+import { graphqlHTTP } from "express-graphql";
+
+const app = express();
+schema;
+app.get("/", (req, res) => {
+  res.send("up and running graphQL");
+});
+
+const root = resolvers;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+    rootValue: root,
+  })
+);
+
+app.listen(8000, () => {
+  console.log("running at 8000");
+});
